c43ccdb1c66bcfc84c33995ffeaf37cada04988a
edermelo-git <eder.worksites@gmail.com>
2025-08-12 17:51:25 -0300

    Change and correction of get(users)

--- server.js
+++ server.js
@@ -1,29 +1,36 @@
-import express from 'express';
+import express, { json } from 'express';
 import { PrismaClient } from '../generated/prisma/index.js';
+import { join } from '@prisma/client/runtime/library';
 
 const app = express();
 const prisma = new PrismaClient();
 
-app.use(express.json()); 
+app.use(express.json());
 
 app.post('/users', async (req, res) => {
-  try {
-    console.log('Body recebido:', req.body); 
-    const { name, email, age } = req.body;
+    try {
+        console.log('Body recebido:', req.body);
+        const { name, email, age } = req.body;
 
-    const user = await prisma.user.create({
-      data: { name, email, age }
-    });
+        const user = await prisma.user.create({
+            data: { name, email, age }
+        });
 
-    res.status(201).json(user);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Erro ao criar usuário' });
-  }
+        res.status(201).json(user);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Erro ao criar usuário' });
+    }
 });
 
-app.get('/users', (req, res) => {
-  res.send('Lista de usuários');
+app.get('/users', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Erro ao buscar usuários:', error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
 });
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
