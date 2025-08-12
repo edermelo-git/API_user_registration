22afdb5ad556603d99b5c5a4e55dc94f836b16fa
edermelo-git <eder.worksites@gmail.com>
2025-08-12 09:44:35 -0300

    Automatic update of servers.js + Tests

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ import express from 'express'
 
 const app = express()
 
+const users = []
+
+app.post('/user',(req, res) => {
+
+    console.log(req)
+    res.send('Ok post')
+
+})
+
 app.get('/user', (req, res) => {
-    res.send('ok,deu bom')    
+    res.send('ok,deu bom')
 })
 
 app.listen(2501)
