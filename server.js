771a868ac121d08ac71a7bb3e330f46d69f53773
edermelo-git <eder.worksites@gmail.com>
2025-08-14 01:33:15 -0300

    DELETE added and tested

--- server.js
+++ server.js
@@ -7,35 +7,35 @@ const prisma = new PrismaClient();
 app.use(express.json());
 
 app.post('/users', async (req, res) => {
-    try {
-        console.log('Body recebido:', req.body);
-        const { name, email, age } = req.body;
+  try {
+    console.log('Body recebido:', req.body);
+    const { name, email, age } = req.body;
 
-        const user = await prisma.user.create({
-            data: { name, email, age }
-        });
+    const user = await prisma.user.create({
+      data: { name, email, age }
+    });
 
-        res.status(201).json(user);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error creating user' });
-    }
+    res.status(201).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error creating user' });
+  }
 });
 
 app.get('/users', async (req, res) => {
-    try {
-        const users = await prisma.user.findMany();
-        res.status(200).json(users);
-    } catch (error) {
-        console.error('Error when searching for users:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error when searching for users:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.put('/users/:id', async (req, res) => {
   const { id } = req.params;
   const { email, name, age } = req.body;
-
+  
   try {
     const existingUser = await prisma.user.findUnique({ where: { email } });
     if (existingUser && existingUser.id !== id)
@@ -48,5 +48,18 @@ app.put('/users/:id', async (req, res) => {
   }
 });
 
+app.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await prisma.user.delete({
+      where: { id },
+    });
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).json({ error: 'Error deleting user' });
+  }
+});
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
